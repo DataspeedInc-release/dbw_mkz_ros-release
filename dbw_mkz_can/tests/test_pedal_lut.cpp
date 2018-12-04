d2cb9f1b41d8d60f25194a606c8f9a4f3b6746e8
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2018-12-04 10:29:10 -0500

    Rebase from 'upstream'

--- dbw_mkz_can/tests/test_pedal_lut.cpp
+++ dbw_mkz_can/tests/test_pedal_lut.cpp
@@ -109,6 +109,25 @@ TEST(pedal_lut, throttlePedalFromPercent)
   EXPECT_NEAR((float)0.606, throttlePedalFromPercent(0.7), (float)0.001);
 }
 
+// Test converting from throttle pedal position to throttle percent
+TEST(pedal_lut, throttlePercentFromPedal)
+{
+  // Out of range
+  EXPECT_EQ((float)0.0, throttlePercentFromPedal(-INFINITY));
+  EXPECT_EQ((float)0.0, throttlePercentFromPedal(-1));
+  EXPECT_EQ((float)1.0, throttlePercentFromPedal(1));
+  EXPECT_EQ((float)1.0, throttlePercentFromPedal(INFINITY));
+
+  // Extreme values
+  EXPECT_EQ((float)0.0, throttlePercentFromPedal(0.15));
+  EXPECT_EQ((float)1.0, throttlePercentFromPedal(0.80));
+
+  // Normal values
+  EXPECT_NEAR((float)0.3, throttlePercentFromPedal(0.347), (float)0.001);
+  EXPECT_NEAR((float)0.5, throttlePercentFromPedal(0.477), (float)0.001);
+  EXPECT_NEAR((float)0.7, throttlePercentFromPedal(0.606), (float)0.001);
+}
+
 int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
