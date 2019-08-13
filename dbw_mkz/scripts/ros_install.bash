4893276ad790300853f1d307c5e2ea3375eb0bdf
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2019-08-13 14:58:21 -0400

    Rebase from 'upstream'

--- dbw_mkz/scripts/ros_install.bash
+++ dbw_mkz/scripts/ros_install.bash
@@ -43,7 +43,7 @@ fi
 # Install ROS
 echo "Installing ROS $ROS_DISTRO..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get install -y ros-$ROS_DISTRO-desktop
 sudo rosdep init
