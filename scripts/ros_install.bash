cb1e69908f7527e47518ec9b1bbc67e29d57cc39
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2022-12-01 17:17:07 -0500

    Rebase from 'release/melodic/dbw_mkz'

--- scripts/ros_install.bash
+++ scripts/ros_install.bash
@@ -45,7 +45,8 @@ fi
 # Install ROS
 echo "Installing ROS $ROS_DISTRO..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y ros-$ROS_DISTRO-desktop
 sudo rosdep init
